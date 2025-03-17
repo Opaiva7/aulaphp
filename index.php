8e37116e2284bd8fef538f136e47ec01a346bbbd
Vinicius Paiva <viniciusspaiva1628@gmail.com>
2025-03-16 21:42:54 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>PRIMEIRA AULA</title>
+</head>
+
+<body>
+    <form action="teste.php" method="post">
+        
+        Nome: <input type="text" name="nome">
+        Idade: <input type="text" name="idade">
+        <input type="submit" value="validar">
+    </form>
+</body>
+
+</html>
---
+++ teste.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo 'cagar meu fi'
+
+?>
